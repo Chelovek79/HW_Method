9a87f453a65824a8f35df70b8c233abbc136bce4
Vit <privit@rambler.ru>
2022-12-09 00:36:46 +0300

    Удалил - String srocDostavki;

--- src/DostavkaBankCard.java
+++ src/DostavkaBankCard.java
@@ -2,21 +2,19 @@ import java.util.Scanner;
 
 public class DostavkaBankCard {
     public static String determinationDeliveryTime (int deliveryDistance) {
-        String srocDostavki;
         if (deliveryDistance > 0 && deliveryDistance < 20) {
-            return srocDostavki = "1 день.";
+            return "1 день";
         }
         if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            return srocDostavki = "2 дня.";
+            return "2 дня.";
         }
         if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            return srocDostavki = "3 дня.";
+            return "3 дня.";
         }
         if (deliveryDistance > 100) {
-            return srocDostavki = "по отдельной договорённости.";
-        } return srocDostavki = " зайдите ;)))";
+            return "по отдельной договорённости.";
+        } return " зайдите ;)))";
     }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Укажите расстояние ('+', '-' км) от нашего офиса до адреса доставки: ");
